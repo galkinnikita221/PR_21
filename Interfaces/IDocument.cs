d2cca75371e0b64aabc0bcdec81fc7d721e585c6
student-a310 <student-a310@PC-A310-9>
2023-11-30 11:29:36 +0500

    Реализованные интерфейсы. Galkin.

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Galkin.Interfaces
+{
+	public interface IDocument
+	{
+		void Save(bool Update = false);
+
+		List<Documents_Galkin.classes.DocumentContext> AllDocuments();
+		void Delete();
+	}
+}
