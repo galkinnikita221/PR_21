fafcac9b5b85b9223c687e5a8d398cf92df51f22
Nikita <galkinnikita228@gmail.com>
2023-12-01 22:13:33 +0500

    Реализованная страница Main. Galkin

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Documents_Galkin.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Documents_Galkin.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public void CreatedUI()
+        {
+            parent.Children.Clear();
+            foreach (DocumentContext document in MainWindow.init.AllDocuments)
+                parent.Children.Add(new Elements.Item(document));
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+
+        private void AddDocuments(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.add);
+        }
+    }
+}
