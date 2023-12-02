74291d90c0448b1d3dc929f78d34ef951a5c16b5
student-a210 <student-a210@PC-A210-4>
2023-12-02 10:41:53 +0500

    Выполненная работа на оценку 3. Galkin

--- Pages/Add.xaml.cs
+++ Pages/Add.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.IO;
+using Microsoft.Win32;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
 
 namespace Documents_Galkin.Pages
 {
@@ -20,13 +12,117 @@ namespace Documents_Galkin.Pages
     /// </summary>
     public partial class Add : Page
     {
-        public Add()
+        public Model.Document Document;
+        public string s_src = "";
+        public Add(Model.Document Document = null)
         {
             InitializeComponent();
+            if (Document != null)
+            {
+                this.Document = Document;
+                if (File.Exists(Document.src))
+                {
+                    s_src = Document.src;
+                    src.Source = new BitmapImage(new Uri(s_src));
+                }
+                tb_name.Text = this.Document.name;
+                tb_user.Text = this.Document.user;
+                tb_id.Text = this.Document.id_document;
+                tb_date.Text = this.Document.date.ToString("dd.MM.yyyy");
+                tb_status.SelectedIndex = this.Document.status;
+                tb_vector.Text = this.Document.vector;
+                bthAdd.Content = "Изменить";
+            }
         }
 
-        private void Back(object sender, RoutedEventArgs e)
+        private void Back(object sender, RoutedEventArgs e) => 
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+
+        private void SelectImage(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "PNG (*.png)|*All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName != "")
+            {
+                src.Source = new BitmapImage(new System.Uri(openFileDialog.FileName));
+                s_src = openFileDialog.FileName;
+            }
+        }
+
+        private void AddDocument(object sender, RoutedEventArgs e)
+        {
+            if (s_src.Length == 0)
+            {
+                MessageBox.Show("Выберите изображение");
+                return;
+            }
+            if (tb_name.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите наименование");
+                return;
+            }
+            if (tb_user.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите ответственного");
+                return;
+            }
+            if (tb_id.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите код");
+                return;
+            }
+            if (tb_date.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите дату поступления");
+                return;
+            }
+            if (tb_status.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите статус");
+                return;
+            }
+            if (tb_vector.Text.Length == 0)
+            {
+                MessageBox.Show("Укажите направление");
+                return;
+            }
+            if (Document == null)
+            {
+                classes.DocumentContext newDocument = new classes.DocumentContext();
+                newDocument.src = s_src;
+                newDocument.name = tb_name.Text;
+                newDocument.user = tb_user.Text;
+                newDocument.id_document = tb_id.Text;
+
+                DateTime newDate = new DateTime();
+                DateTime.TryParse(tb_date.Text, out newDate);
+                newDocument.date = newDate;
+                newDocument.status = tb_status.SelectedIndex;
+                newDocument.vector = tb_vector.Text;
+                newDocument.Save();
+                MessageBox.Show("Документ добавлен");
+            }
+            else
+            {
+                classes.DocumentContext newDocument = new classes.DocumentContext();
+                newDocument.src = s_src;
+                newDocument.id = Document.id;
+                newDocument.name = tb_name.Text;
+                newDocument.user = tb_user.Text;
+                newDocument.id_document = tb_id.Text;
+
+                DateTime newDate = new DateTime();
+                DateTime.TryParse(tb_date.Text, out newDate);
+                newDocument.date = newDate;
+                newDocument.status = tb_status.SelectedIndex;
+                newDocument.vector = tb_vector.Text;
+                newDocument.Save();
+                MessageBox.Show("Документ изменён<>");
+            }
+            MainWindow.init.AllDocuments = new classes.DocumentContext().AllDocuments();
             MainWindow.init.OpenPages(MainWindow.pages.main);
         }
     }
